ca2414a67eae314fd313a95c897158ab59033b0a
OlegK57 <oleg.kasyanov@forthscale.com>
2017-12-11 11:06:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+ls -la /var/secrets
 echo "Prepare secret environments"
 if [ -d "/var/secrets" ]; then
   for a in /var/secrets/*
   do 
-    echo $(basename $a)="$(cat $a)"
+    echo $a
+    echo $(basename $a) "$(cat $a)"
     export $(basename $a)="$(cat $a)"
   done
 fi
